dedf1414657dc4f725e0e702fde95efce9309b27
paul1124 <paulhong1124@gmail.com>
2020-01-05 14:10:41 -0800

    reinitialize

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="app">
+
+        </div>
+    )
+}
+
+export default App;
